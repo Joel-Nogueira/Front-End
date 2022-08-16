ad3bd690c94499e77a0c56e3d7f84475556a841c
Joel-Nogueira <joelnogueira733@gmail.com>
2022-08-16 20:41:08 -0300

    Inclusão do Firebase ao projeto e criação da autenticação

---
+++ eventos/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB1A8dQctGD_fpduFuJYZ7hz3qwoWL8t8Q",
+  authDomain: "eventos-28948.firebaseapp.com",
+  projectId: "eventos-28948",
+  storageBucket: "eventos-28948.appspot.com",
+  messagingSenderId: "320819513509",
+  appId: "1:320819513509:web:f6ffc6b1aa07cbd351a54c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- eventos/src/view/login/index.js
+++ eventos/src/view/login/index.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.css';
 
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function logar() {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, senha)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                alert('DEU CERTO');
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert('NÃO DEU CERTO');
+            });
+    }
+
     return (
         <div className='login-content d-flex align-items-center'>
             <form className='mx-auto'>
                 <h1 className="h3 mb-3 fw-normal text-center text-white fw-bold">Login</h1>
 
                 <div className="form-floating my-2">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="Email" />
+                    <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="floatingInput" placeholder="Email" />
                     <label for="floatingInput">Email</label>
                 </div>
 
                 <div className="form-floating my-2">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
+                    <input onChange={(e) => setSenha(e.target.value)} type="password" className="form-control" id="floatingPassword" placeholder="Senha" />
                     <label for="floatingPassword">Senha</label>
                 </div>
 
-                <button className="w-100 btn btn-lg btn-login" type="submit">Sign in</button>
+                <button onClick={logar} className="w-100 btn btn-lg btn-login" type="button">Logar</button>
 
                 <div className='msg-login text-white text-center my-5'>
                     <span><strong>Login realizado.</strong> Você está conectado.</span>
-                    <br/>
+                    <br />
                     <span><strong>Ocorreu um erro.</strong> Verifique se a senha ou o usuário está correto.</span>
                 </div>
 
